a8715e9f956ea5575ba2564f53b0e5cf9ca007a3
joellegg <joellegg@bellsouth.net>
2017-04-24 14:27:27 -0500

    added start script,  working on animal controller

--- controllers/animalsCtrl.js
+++ controllers/animalsCtrl.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const Animal = require('../models/animal')
+const Animal = require('../models/animalsMdl')
 
 module.exports.getAnimal = ({ params: { id } }, res, next) => {
-  Animal.getSingleShow(id)
+  Animal.getSingleAnimal(id)
   .then(animal => res.status(200).json(animal))
   .catch(err => next(err))
 }
@@ -14,14 +14,14 @@ module.exports.getAnimals = (req, res, next) => {
   .catch(err => next(err))
 }
 
-module.exports.getAnimalTricks = ({ params: { id }}, res, next) => {
-  Animal.
-}
+// module.exports.getAnimalTricks = ({ params: { id }}, res, next) => {
+//   Animal.
+// }
 
-module.exports.getAnimalTrainers = ({ params: { id }}, res, next) => {
-  Animal.
-}
+// module.exports.getAnimalTrainers = ({ params: { id }}, res, next) => {
+//   Animal.
+// }
 
-module.exports.getAnimalKeepers = ({ params: { id }}, res, next) => {
-  Animal.
-}
+// module.exports.getAnimalKeepers = ({ params: { id }}, res, next) => {
+//   Animal.
+// }
--- routes/index.js
+++ routes/index.js
@@ -8,16 +8,16 @@ router.use(require('./routes'))
 router.get('/', function(req, res) {
   res.json({
     "title": "Menagerie API data",
-    "animal-tricks": "<name>.herokuapp.com/api/v1/animals/:id/tricks",
-    "animal-trainers": "<name>.herokuapp.com/api/v1/animals/:id/trainers",
-    "animal-keepers": "<name>.herokuapp.com/api/v1/animals/:id/keepers",
-    "keepers-animals": "<name>.herokuapp.com/api/v1/keepers/:id/animals",
-    "trainers-animals": "<name>.herokuapp.com/api/v1/trainers/:id/animals"
-    "trainers-tricks": "<name>.herokuapp.com/api/v1/trainers/:id/tricks"
-    "tricks-animals": "<name>.herokuapp.com/api/v1/tricks/:id/animals"
-    "tricks-trainers": "<name>.herokuapp.com/api/v1/tricks/:id/trainers"
-    "types-animals": "<name>.herokuapp.com/api/v1/types/:id/animals"
-    "types-trainers": "<name>.herokuapp.com/api/v1/types/:id/trainers"
+    // "animal-tricks": "<name>.herokuapp.com/api/v1/animals/:id/tricks",
+    // "animal-trainers": "<name>.herokuapp.com/api/v1/animals/:id/trainers",
+    // "animal-keepers": "<name>.herokuapp.com/api/v1/animals/:id/keepers",
+    // "keepers-animals": "<name>.herokuapp.com/api/v1/keepers/:id/animals",
+    // "trainers-animals": "<name>.herokuapp.com/api/v1/trainers/:id/animals"
+    // "trainers-tricks": "<name>.herokuapp.com/api/v1/trainers/:id/tricks"
+    // "tricks-animals": "<name>.herokuapp.com/api/v1/tricks/:id/animals"
+    // "tricks-trainers": "<name>.herokuapp.com/api/v1/tricks/:id/trainers"
+    // "types-animals": "<name>.herokuapp.com/api/v1/types/:id/animals"
+    // "types-trainers": "<name>.herokuapp.com/api/v1/types/:id/trainers"
   })
 })
 
