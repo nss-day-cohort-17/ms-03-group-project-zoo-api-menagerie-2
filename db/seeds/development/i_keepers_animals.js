7d0f6e6c93208441a629ec8c28dafb6011fa3d4e
joellegg <joellegg@bellsouth.net>
2017-04-26 09:09:28 -0500

    fixed database

--- db/seeds/development/i_keepers_animals.js
+++ db/seeds/development/i_keepers_animals.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const { knex } = require('../../../db/database');
-const keepersAnimalsData = require('./trainers_tricks')
+const keepersAnimalsData = require('./keepers_animals')
 
 let keepersAnimalsPromise = keepersAnimalsData.map((res) => {
-  return knex('trainers_tricks').insert({keeper_id:res.keeper_id, animal_id:res.animal_id})
+  return knex('keepers_animals').insert({keeper_id:res.keeper_id, animal_id:res.animal_id})
 })
 
 exports.seed = function(knex, Promise) {
-  return knex('trainers_tricks').del()
+  return knex('keepers_animals').del()
   .then(function() {
     return Promise.all(keepersAnimalsPromise)
   })
